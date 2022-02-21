ce0bda8a4e107157f6bd8b6f46f1f6d171d4cab4
byjasonrm <99998925+byjasonrm@users.noreply.github.com>
2022-02-21 04:02:14 +0200

    1.0.0

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
     $(".menu-close").click(function(){
         $("menu").removeClass("show-menu");
     });
+
+
+    $(window).scroll(function(){
+        let value = $(window).scrollTop();
+
+        $("#stars").css('top', -value*0.5 + 'px');
+        $("#plane").css('left', -value*0.5 + 'px');
+        $("#title").css('top', -value*0.15 + 'px');
+        $("#city-two").css('top', value*0.15 + 'px');
+        $("#city-one").css('top', -value*0.05 + 'px');
+    });
 });
