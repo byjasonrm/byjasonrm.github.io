204d145b793ee905c95d170766fcefa4dff99093
byjasonrm <99998925+byjasonrm@users.noreply.github.com>
2022-02-21 21:03:53 +0200

    1.0.0

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+
+    if($(window).height() >= $(window).width()){
+        MobileImages();
+    }else{
+        DesktopImages();
+    }
+
     $(".menu").click(function(){
         if(!$("menu").hasClass("show-menu")){
             $("menu").addClass("show-menu");
@@ -11,12 +18,56 @@ $(document).ready(function(){
 
 
     $(window).scroll(function(){
-        let value = $(window).scrollTop();
+        let scroll = $(window).scrollTop();
 
-        $("#stars").css('top', -value*0.5 + 'px');
-        $("#plane").css('left', -value*0.5 + 'px');
-        $("#title").css('top', -value*0.15 + 'px');
-        $("#city-two").css('top', value*0.2 + 'px');
+        $("#stars").css('top', -scroll*0.5 + 'px');
+        $("#plane").css('left', -scroll*0.5 + 'px');
+        $("#title").css('top', -scroll*0.15 + 'px');
+        $("#city-two").css('top', scroll*0.2 + 'px');
         // $("#city-one").css('top', -value*0.1 + 'px');
+
+        if(scroll > $(window).height()/2){
+            $(".hero-content").addClass("reveal-animation");
+        }
+
+        if(scroll > ($(window).height()*2)/2){
+            $(".readfree-content").addClass("reveal-animation");
+        }
+
+        if(scroll > $(window).height()){
+            $("nav").addClass("nav-active");
+        }else{
+            $("nav").removeClass("nav-active");
+        }
+    });
+
+
+    $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.height() >= win.width()) { 
+            MobileImages();
+        }else{
+            DesktopImages();
+        }
     });
 });
+
+function MobileImages(){
+    $("#bg").attr("src","/assets/images/cover/bg.svg");
+    $("#stars").attr("src","/assets/images/cover/stars.png");
+    $("#plane").attr("src","/assets/images/cover/plane.svg");
+    $("#city-two").attr("src","/assets/images/cover/city-two.svg");
+    $("#city-one").attr("src","/assets/images/cover/city-one.svg");
+    $("#main").attr("src","/assets/images/cover/main.svg");
+    $("#title").attr("src","/assets/images/cover/title.png");
+}
+
+function DesktopImages(){
+    $("#bg").attr("src","/assets/images/cover/horizontal/bg.svg");
+    $("#stars").attr("src","/assets/images/cover/horizontal/stars.png");
+    $("#plane").attr("src","/assets/images/cover/horizontal/plane.svg");
+    $("#city-two").attr("src","/assets/images/cover/horizontal/city-two.svg");
+    $("#city-one").attr("src","/assets/images/cover/horizontal/city-one.svg");
+    $("#main").attr("src","/assets/images/cover/horizontal/main.svg");
+    $("#title").attr("src","/assets/images/cover/horizontal/title.png");
+}
